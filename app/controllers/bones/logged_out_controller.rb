49c1242d2107d90b1ff55d5ed7e009261944650f
James McCarthy <james2mccarthy@gmail.com>
2011-08-08 12:11:37 +0100

    Updated the rendering of the example elements.

--- app/controllers/bones/logged_out_controller.rb
+++ app/controllers/bones/logged_out_controller.rb
@@ -1,3 +1,7 @@
 class Bones::LoggedOutController < LoggedOutController
 
+  def elements
+    @pages = Bones::Example.wibble.page(params[:page] || 1)
+  end
+  
 end
--- app/models/bones/example.rb
+++ app/models/bones/example.rb
@@ -1,40 +1,61 @@
-class Example < ActiveRecord::Base
+module Bones
+  class Example < ActiveRecord::Base
 
-  establish_connection(:adapter => "sqlite3", :database => ":memory:")
-  after_initialize :add_error_message_to_field
+    establish_connection(:adapter => "sqlite3", :database => ":memory:")
+  
+    after_initialize :add_error_message_to_field
 
-  ActiveRecord::Migration.class_eval do
+    ActiveRecord::Migration.class_eval do
 
-    def self.connection
-      Example.connection
+      def self.connection
+        Example.connection
+      end
+
+      create_table :examples, :force => true do |t|
+        t.timestamps
+        t.string    :email
+        t.string    :password
+        t.string    :required
+        t.string    :optional
+        t.string    :disabled
+        t.string    :error
+        t.string    :no_label
+        t.text      :text_area
+        t.boolean   :check_box, :default => true
+        t.boolean   :radio, :default => true
+        t.string    :radio_collection
+        t.string    :check_box_collection
+        t.string    :select_collection
+      end
+    
     end
 
-    create_table :examples, :force => true do |t|
-      t.timestamps
-      t.string    :email
-      t.string    :password
-      t.string    :required
-      t.string    :optional
-      t.string    :disabled
-      t.string    :error
-      t.string    :no_label
-      t.text      :text_area
-      t.boolean   :check_box, :default => true
-      t.boolean   :radio, :default => true
-      t.string    :radio_collection
-      t.string    :check_box_collection
-      t.string    :select_collection
+  
+    paginates_per 5
+  
+    validates_presence_of :required
+    
+    def add_error_message_to_field
+      self.errors.add :error, 'Has a validation error message'
     end
 
   end
+  
+  
+  module ExamplePagination
 
-  validates_presence_of :required
-
-  def add_error_message_to_field
-    self.errors.add :error, 'Has a validation error message'
+    def wibble
+      where(:required => 'Yep').order(:email)
+    end
+    
   end
-
+  
+  Example.extend(Bones::ExamplePagination)
+  
+  50.times { |n| Bones::Example.create(:required => 'Yep', :email => "email-#{n}@example.com") }
+  
 end if Rails.env == 'development'
+
 # == Schema Information
 #
 # Table name: examples
