e191621e865a676b04a9e42dc020d57883da3231
James McCarthy <james2mccarthy@gmail.com>
2012-05-14 23:12:26 +0100

    Updated the controller_spec template.

--- lib/generators/bones/install_generator.rb
+++ lib/generators/bones/install_generator.rb
@@ -3,7 +3,7 @@ module Bones
 
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
-      argument :roles, :type => :array, :default => "admin", :desc => 'The roles to create controller specs for in the template'
+      argument :roles, :type => :array, :default => ["admin"], :desc => 'The roles to create controller specs for in the template'
     
       def copy_controller_file
         copy_file "controller.rb", "lib/templates/rails/scaffold_controller/controller.rb"
