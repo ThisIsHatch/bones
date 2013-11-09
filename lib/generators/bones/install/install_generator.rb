ec9bb6d218e5207d36fa7f0e3c75d6de14e16512
James McCarthy <james@thisishatch.co.uk>
2013-11-09 17:20:12 -0500

    Updated install generator to add and example create_event and event wireframe.
    Signed-off-by: James McCarthy <james@thisishatch.co.uk>

--- lib/generators/bones/install/install_generator.rb
+++ lib/generators/bones/install/install_generator.rb
@@ -10,6 +10,8 @@ module Bones
       def install_steps
         route "mount Bones::Engine => '/bones' unless Rails.env.production?"
         copy_file 'example.html.erb', 'app/views/bones/wireframes/example.html.erb'
+        copy_file 'create_event.html.erb', 'app/views/bones/wireframes/create_event.html.erb'
+        copy_file 'event.html.erb', 'app/views/bones/wireframes/event.html.erb'
         copy_file 'components.html.erb', 'app/views/bones/components/index.html.erb'
       end
     end
