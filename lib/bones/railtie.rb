737d259258274855f78dba074cca9e98327eac56
James McCarthy <james@thisishatch.co.uk>
2013-12-03 23:36:08 -0500

    Ensured Kss only included in development.
    Signed-off-by: James McCarthy <james@thisishatch.co.uk>

--- lib/bones/railtie.rb
+++ lib/bones/railtie.rb
@@ -6,7 +6,7 @@ module Bones
 
     initializer "canard.active_record" do |app|
       ActiveSupport.on_load :action_controller do
-        include Kss::ApplicationHelper if defined?(Kss)
+        include Kss::ApplicationHelper if defined?(Kss) && Rails.env.development?
       end
     end
   end
