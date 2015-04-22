287dbf4f483b66d45cd4229f15a1089bae11d6c6
James McCarthy <james@thisishatch.co.uk>
2015-04-22 15:51:09 +0700

    Move components page into the correct namespace.
    Signed-off-by: James McCarthy <james@thisishatch.co.uk>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Bones::Engine.routes.draw do
-  get 'components' => 'components#index'
+  get 'components' => 'bones/components#index'
   get 'wireframes/(:action(.:format))', :controller => 'bones/wireframes'
 end
