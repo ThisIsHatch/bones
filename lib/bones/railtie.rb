30c7464c5840bec9d624579f4bc35f5830468754
James McCarthy <james@thisishatch.co.uk>
2015-05-15 19:25:17 +0700

    Hack to ensure locales are always reloaded in dev.
    Signed-off-by: James McCarthy <james@thisishatch.co.uk>

--- lib/bones/railtie.rb
+++ lib/bones/railtie.rb
@@ -4,10 +4,16 @@ require 'rails'
 module Bones
   class Railtie < Rails::Railtie
 
-    initializer "canard.active_record" do |app|
-      ActiveSupport.on_load :action_controller do
-        include Kss::ApplicationHelper if defined?(Kss) && Rails.env.development?
-      end
+    config.reload_classes_only_on_change = false
+
+    config.watchable_dirs['/config/locales'] = ['.yml']
+
+    config.to_prepare do
+      i18n_path = I18n.load_path
+      app_path = Dir[File.join(Rails.root, 'config', 'locales', '**/*.yml')]
+      new_path = (i18n_path | app_path).keep_if { |file| File.exists?(file) }
+      I18n.load_path = new_path
+      I18n.reload!
     end
   end
 end
